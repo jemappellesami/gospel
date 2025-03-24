13c9baf66bd58383a07ab23107611e5916950bad
Sami AS <samiabdulsater@gmail.com>
2025-03-24 09:46:39 +0000

    Modify Readme

--- gospel/post_processing/main.py
+++ gospel/post_processing/main.py
@@ -124,14 +124,16 @@ plt.plot(p_values, [proportion_wrong_outcomes_dict[prob] for prob in p_values])
 
 readmefile = folder + "/README.md"
 with open(readmefile, "w") as file:
-    with open("circuits/README.md", "r") as circuits_readme_file :
-        circuits_readme_lines =  circuits_readme_file.readlines()
+    content = """
+    These results have been generated in lines with the following settings:
 
-        with open("gospel/cluster/sampled_circuits.txt", "r") as sampled_circuits_file:
-            sampled_circuits_lines = sampled_circuits_file.readlines()
-
-            lines = [circuits_readme_lines,
-                     sampled_circuits_lines
-            ]
-            for line in circuits_readme_lines + sampled_circuits_lines:
-                file.write(line)
+    ```
+    git checkout 629616845598b2c8b932a9014798d0b6989caede
+    rm -rf circuits
+    python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
+    git checkout sim-verif
+    mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
+    cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
+    ```
+    """
+    file.write(content)
