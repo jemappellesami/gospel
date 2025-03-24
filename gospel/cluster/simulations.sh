f4f88ca20cbf8dfd7786283684e70808c28677c6
Sami AS <samiabdulsater@gmail.com>
2025-03-24 18:35:02 +0000

    attempt to fix uncor depol

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -12,14 +12,30 @@ n_comp_run=100
 n_test_run=100
 n_nodes=$n_instances
 
-# echo "Simulations for Gentle Global Noise on h-oll circuits"
-# # Generate circuits and samples from h-oll
-# git checkout 629616845598b2c8b932a9014798d0b6989caede
-# rm -rf circuits
-# python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
-# git checkout sim-verif
-# mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
-# cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
+echo "Simulations on h-oll circuits"
+# Generate circuits and samples from h-oll
+git checkout 629616845598b2c8b932a9014798d0b6989caede
+rm -rf circuits
+python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
+git checkout sim-verif
+mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
+cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
+
+echo "MALICIOUS"
+# Malicious model
+for p_err in 0.01 0.05 0.08 ; do
+  PORT=24396
+
+#   # Print p and assigned port
+#   echo "Running with p_err=$p_err, PORT=$PORT"
+
+  # Run the process in the background locally
+  # nohup python -m gospel.cluster.run_veriphix-malicious $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale 12 & 
+
+  # Run the process in the background on the cluster
+  python -m gospel.cluster.run_veriphix-malicious $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes &
+
+done
 
 # # Gentle Global Noise
 # for p_err in 0.05 0.10 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 1.00 ; do
@@ -32,18 +48,6 @@ n_nodes=$n_instances
 #   # time python -m gospel.cluster.run_veriphix $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale 12  
 
 #   # Run the process in the background on the cluster
-#   (time python -m gospel.cluster.run_veriphix $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes) 2>> exec_times.log
-
-# done
-# echo "GENTLE GLOBAL NOISE (remaining)"
-# Gentle global noise
-# for p_err in 0.7 ; do
-#   PORT=24395
-
-   # Print p and assigned port
-#   echo "Running with p_err=$p_err, PORT=$PORT"
-
-#   # Run the process in the background
 #   (time python -m gospel.cluster.run_veriphix $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes) 2>> exec_times.log
 
 # done
@@ -64,21 +68,7 @@ n_nodes=$n_instances
 
 # done
 
-# echo "MALICIOUS"
-# # Malicious model
-# for p_err in 0.01 0.05 0.08 0.11 0.15 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 1.00 ; do
-#   PORT=24396
 
-# #   # Print p and assigned port
-# #   echo "Running with p_err=$p_err, PORT=$PORT"
-
-#   # Run the process in the background locally
-#   # nohup python -m gospel.cluster.run_veriphix-malicious $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale 12 & 
-
-#   # Run the process in the background on the cluster
-#   nohup python -m gospel.cluster.run_veriphix-malicious $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes &
-
-# done
 
 
 
@@ -95,7 +85,7 @@ n_nodes=$n_instances
 
 # done
 
-echo "Simulations on SAS circuits"
+# echo "Simulations on SAS circuits"
 # git checkout 143645c8cedcd2c0e14dd58e991b610ad7385d7a
 # rm -rf circuits
 # python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
@@ -104,20 +94,20 @@ echo "Simulations on SAS circuits"
 # cp gospel/cluster/sampled_circuits.sas.txt gospel/cluster/sampled_circuits.txt
 # echo "DEPOLARIZING (UNCORRELATED)"
 # Depolarizing
-for p_err in 0.00005 0.00009 ; do
-  PORT=35407
+# for p_err in 0.00005 0.00009 ; do
+#   PORT=35407
 
-  # Print p and assigned port
-  echo "Running with p_err=$p_err, PORT=$PORT"
+#   # Print p and assigned port
+#   echo "Running with p_err=$p_err, PORT=$PORT"
 
-  # Run the process in the background
-  time python -m gospel.cluster.run_veriphix-uncorr_depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 10 --memory 4 --cores 4 --port $PORT --scale $n_nodes &
+#   # Run the process in the background
+#   time python -m gospel.cluster.run_veriphix-uncorr_depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 10 --memory 4 --cores 4 --port $PORT --scale $n_nodes &
 
-done
+# done
 
-wait  # Ensure all background jobs complete
+# wait  # Ensure all background jobs complete
 
-echo "All jobs completed!"
+# echo "All jobs completed!"
 
 
 # Loaded 100 circuits.
