8a7abbd61bd88ba273f831c8badaf10a1e538fb8
Sami AS <samiabdulsater@gmail.com>
2025-03-24 18:10:23 +0000

    attempt to fix uncor depol

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -96,13 +96,13 @@ n_nodes=$n_instances
 # done
 
 echo "Simulations on SAS circuits"
-git checkout 143645c8cedcd2c0e14dd58e991b610ad7385d7a
-rm -rf circuits
-python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
-git checkout sim-verif
-mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
-cp gospel/cluster/sampled_circuits.sas.txt gospel/cluster/sampled_circuits.txt
-echo "DEPOLARIZING (UNCORRELATED)"
+# git checkout 143645c8cedcd2c0e14dd58e991b610ad7385d7a
+# rm -rf circuits
+# python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
+# git checkout sim-verif
+# mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
+# cp gospel/cluster/sampled_circuits.sas.txt gospel/cluster/sampled_circuits.txt
+# echo "DEPOLARIZING (UNCORRELATED)"
 # Depolarizing
 for p_err in 0.000005 0.000009 ; do
   PORT=35407
