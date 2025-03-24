1324d08a4ff742d71dfff3668780e5920e5d60a4
Sami AS <samiabdulsater@gmail.com>
2025-03-24 15:03:33 +0000

    Prepare other depol sim

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -111,7 +111,7 @@ for p_err in 0.0009 0.0008 0.0007 0.0018 ; do
   echo "Running with p_err=$p_err, PORT=$PORT"
 
   # Run the process in the background
-  (time python -m gospel.cluster.run_veriphix-uncorr_depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 10 --memory 4 --cores 4 --port $PORT --scale $n_nodes) 2>> exec_times.log
+  time python -m gospel.cluster.run_veriphix-uncorr_depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 10 --memory 4 --cores 4 --port $PORT --scale $n_nodes
 
 done
 
