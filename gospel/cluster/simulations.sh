c5a7a38f917b0a041a37fe8d095da8092743f198
Sami AS <samiabdulsater@gmail.com>
2025-03-24 22:11:12 +0000

    Add simulation results for malicious

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -13,12 +13,12 @@ n_nodes=$n_instances
 
 # echo "Simulations on h-oll circuits"
 # # Generate circuits and samples from h-oll
-# git checkout 629616845598b2c8b932a9014798d0b6989caede
-# rm -rf circuits
-# python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
-# git checkout sim-verif
-# mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
-# cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
+git checkout 629616845598b2c8b932a9014798d0b6989caede
+rm -rf circuits
+python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
+git checkout sim-verif
+mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
+cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
 
 # echo "MALICIOUS"
 # # Malicious model
