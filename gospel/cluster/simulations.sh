a667400acb3912a107fd2122c8c29cc6b6387595
Sami AS <samiabdulsater@gmail.com>
2025-03-25 22:36:41 +0000

    Prepare next sim: depol, uncor depool

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -51,21 +51,21 @@ cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
 
 # done
 
-echo "STRONG GLOBAL NOISE"
-# Strong global noise
-for p_err in 0.025 0.05 0.075 0.15; do
-  PORT=24395
+# echo "STRONG GLOBAL NOISE"
+# # Strong global noise
+# for p_err in 0.025 0.05 0.075 0.15; do
+#   PORT=24395
 
-  # Print p and assigned port
-  echo "Running with p_err=$p_err, PORT=$PORT"
+#   # Print p and assigned port
+#   echo "Running with p_err=$p_err, PORT=$PORT"
 
-  # Run the process in the background locally
-  #time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale $n_nodes
+#   # Run the process in the background locally
+#   #time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale $n_nodes
 
-  # Run the process in the background on the cluster
-  python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes &
+#   # Run the process in the background on the cluster
+#   python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes &
 
-done
+# done
 
 
 wait
