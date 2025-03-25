1085cba872a773396196a70f6f4ebc19c56d9440
Sami AS <samiabdulsater@gmail.com>
2025-03-25 11:42:55 +0000

    Add sim analysis for malicious, and top polot

--- gospel/post_processing/main.py
+++ gospel/post_processing/main.py
@@ -157,7 +157,8 @@ plt.savefig(folder + "plot.png")
 
 readmefile = folder + "/README.md"
 with open(readmefile, "w") as file:
-    content = """
+    if noise_type in ["MALICIOUS", "GENTLE GLOBAL", "STRONG GLOBAL"]:
+        content = """
 These results have been generated in lines with the following settings:
 
 ```
@@ -167,6 +168,19 @@ python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 
 git checkout sim-verif
 mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
 cp gospel/cluster/sampled_circuits.holl.txt gospel/cluster/sampled_circuits.txt
+    ```
+"""
+    else:
+        content ="""
+These results have been generated in lines with the following settings:
 ```
-    """
+git checkout 143645c8cedcd2c0e14dd58e991b610ad7385d7a
+rm -rf circuits
+python -m gospel.sampling_circuits.sampling_circuits --ncircuits 1000 --nqubits 5 --depth 30 --p-gate 0.5 --p-cnot 0.25 --p-cnot-flip 0.5 --p-rx 0.5 --seed 1729 --target circuits
+git checkout sim-verif
+mv gospel/cluster/sampled_circuits.txt gospel/cluster/sampled_circuits.tmp.txt 
+cp gospel/cluster/sampled_circuits.sas.txt gospel/cluster/sampled_circuits.txt
+```
+"""
+
     file.write(content)
